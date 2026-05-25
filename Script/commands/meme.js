89eafe1b58a3ba372bd9054a66e1851acc0ba501
md2060100-commits <md2060100@gmail.com>
2026-05-25 21:24:47 +0600

    Update meme.js

--- Script/commands/meme.js
+++ Script/commands/meme.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "meme",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
   description: "Random joke image",
   commandCategory: "Image",
   usages: "joke",
